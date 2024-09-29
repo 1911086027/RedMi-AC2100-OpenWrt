57343a8a5a124d423e01ee9264344f1b9f825038
C_pumpkin <62928527+1911086027@users.noreply.github.com>
2024-09-29 22:31:01 +0800

    Create diy.sh

---
+++ diy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# Add packages
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
+
+echo "
+# 插件
+CONFIG_PACKAGE_ua2f=y
+CONFIG_PACKAGE_luci-app-ua2f=y
+" >> .config
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题
+sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 修改主机名
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
